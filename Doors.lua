94b32cb999b458b9426f68227babce1102ee7247
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-22 22:16:10 +0800

    Create Doors.lua

---
+++ Doors.lua
@@ -0,0 +1,8 @@
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+local cue2 = Instance.new("Sound")
+	cue2.Parent = game.Workspace
+	cue2.Name = "Sound"
+	cue2.SoundId = "rbxassetid://833871080"
+	cue2.Volume = 2
+	cue2.PlaybackSpeed = 1
+	cue2:Play()
